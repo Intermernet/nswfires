732c50b78169c2149d969320355f55e587725b82
Gogs <intermernet@gmail.com>
2020-01-05 07:43:05 +0000

    Add cron script

---
+++ scripts/update_nswfires_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+whoami
+cd ~mike/nswfires
+git pull
+curl http://www.rfs.nsw.gov.au/feeds/majorIncidents.json -o ~mike/nswfires/docs/majorIncidents.json
+git commit -am "Update Incident DB"
+git push gh_nswfires master
+
