c5bbef07f64e848098df96f1963a1d6c96a7e0fd
Mike Hughes <intermernet@gmail.com>
2020-01-05 20:36:34 +1100

    Add command to clean hotspots data

---
+++ scripts/clean_hotspots/clean_hotspots.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+// clean_hotspots removes all non-NSW hotspot entries from the GeoJSON file retrieved from
+// https://hotspots.dea.ga.gov.au/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&outputFormat=application/json&typeNames=public:hotspots_three_days
+
+// GeoJSON represents the GeoJSON format
+type GeoJSON struct {
+	Type           string     `json:"type"`
+	Features       []Features `json:"features"`
+	TotalFeatures  int        `json:"totalFeatures"`
+	NumberMatched  int        `json:"numberMatched"`
+	NumberReturned int        `json:"numberReturned"`
+	TimeStamp      time.Time  `json:"timeStamp"`
+	Crs            Crs        `json:"crs"`
+}
+
+// Geometry fields of GeoJSON
+type Geometry struct {
+	Type        string    `json:"type"`
+	Coordinates []float64 `json:"coordinates"`
+}
+
+// Properties fields of GeoJSON
+type Properties struct {
+	ID                       int         `json:"id"`
+	Satellite                string      `json:"satellite"`
+	SatelliteNssdcID         string      `json:"satellite_nssdc_id"`
+	SatelliteOperatingAgency string      `json:"satellite_operating_agency"`
+	Sensor                   string      `json:"sensor"`
+	Orbit                    int         `json:"orbit"`
+	StartDt                  time.Time   `json:"start_dt"`
+	StopDt                   time.Time   `json:"stop_dt"`
+	Filename                 string      `json:"filename"`
+	ProcessDt                time.Time   `json:"process_dt"`
+	ProcessAlgorithm         string      `json:"process_algorithm"`
+	ProcessAlgorithmVersion  string      `json:"process_algorithm_version"`
+	Product                  string      `json:"product"`
+	LoadDt                   time.Time   `json:"load_dt"`
+	Latitude                 float64     `json:"latitude"`
+	Longitude                float64     `json:"longitude"`
+	TempKelvin               float64     `json:"temp_kelvin"`
+	Datetime                 time.Time   `json:"datetime"`
+	Power                    float64     `json:"power"`
+	Confidence               int         `json:"confidence"`
+	AustralianState          string      `json:"australian_state"`
+	FireCategoryName         interface{} `json:"fire_category_name"`
+	HoursSinceHotspot        float64     `json:"hours_since_hotspot"`
+}
+
+// Features fields of GeoJSON
+type Features struct {
+	Type         string     `json:"type"`
+	ID           string     `json:"id"`
+	Geometry     Geometry   `json:"geometry"`
+	GeometryName string     `json:"geometry_name"`
+	Properties   Properties `json:"properties"`
+}
+
+// CrsProperties fields of GeoJSON
+type CrsProperties struct {
+	Name string `json:"name"`
+}
+
+// Crs fields of GeoJSON
+type Crs struct {
+	Type       string        `json:"type"`
+	Properties CrsProperties `json:"properties"`
+}
+
+func main() {
+	var input = flag.String("input", "input.json", "JSON data to process")
+	var output = flag.String("output", "output.json", "JSON data to ouput")
+
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(*input)
+	if err != nil {
+		log.Fatalf("could not open file for input: %v\n", err)
+	}
+	j := new(GeoJSON)
+	err = json.Unmarshal(data, j)
+	if err != nil {
+		log.Fatalf("could not process JSON data: %v\n", err)
+	}
+	fs := make([]Features, 0)
+	for _, f := range j.Features {
+		if f.Properties.AustralianState == "NSW" {
+			fs = append(fs, f)
+		}
+	}
+	j.Features = fs
+	out, err := json.Marshal(j)
+	if err != nil {
+		log.Fatalf("could not convert JSON data: %v\n", err)
+	}
+	err = ioutil.WriteFile(*output, out, 0644)
+	if err != nil {
+		log.Fatalf("could not write to file %s: %v\n", *output, err)
+	}
+}
---
+++ scripts/clean_hotspots/go.mod
@@ -0,0 +1,3 @@
+module github.com/Intermernet/nswfires/scripts/clean_hotspots
+
+go 1.13
