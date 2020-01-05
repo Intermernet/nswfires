a4849d847700066ea8d6c6b5ef7a0882ed2a600f
Gogs <intermernet@gmail.com>
2020-01-05 10:01:03 +0000

    Update Incident and HotSpot DBs

--- scripts/update_nswfires_data.sh
+++ scripts/update_nswfires_data.sh
@@ -3,6 +3,9 @@
 cd ~mike/nswfires
 git pull
 curl http://www.rfs.nsw.gov.au/feeds/majorIncidents.json -o ~mike/nswfires/docs/majorIncidents.json
-git commit -am "Update Incident DB"
+curl 'https://hotspots.dea.ga.gov.au/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&outputFormat=application/json&typeNames=public:hotspots_three_days' -o ~/mike/nswfires/input.json
+~mike/nswfires/scripts/clean_hotspots/clean_hotspots -input ~mike/nswfires/input.json -output ~mike/nswfires/docs/features.json
+rm -f ~mike/nswfires/input.json
+git commit -am "Update Incident and HotSpot DBs"
 git push gh_nswfires master
 
